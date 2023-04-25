8e82e25c5ceb21f4475f669a5ed789f66ac0231b
Steven Kang <s.kang919@gmail.com>
2023-04-24 19:01:10 -0700

    Basic Client resolver constructor & resolver implementation

--- PongClient/Client.cpp
+++ PongClient/Client.cpp
@@ -1,1 +1,17 @@
 #include "Client.h"
+
+
+Client::Client(asio::io_context& io_context) :
+	io_context(io_context),
+	socket(udp::socket(io_context)) {
+
+	connectionState = Disconnected;
+	sendBuf = array<char, 64>{};
+	recvBuf = array<char, 64>{};
+}
+
+void Client::ResolveReceiverEndpoint() {
+	udp::resolver resolver(io_context);
+
+	receiverEndpoint = *resolver.resolve(udp::v4(), "127.0.0.1", "pong").begin();
+}
--- PongClient/Client.h
+++ PongClient/Client.h
@@ -1,6 +1,8 @@
 #include <boost/asio.hpp>
+#include <boost/array.hpp>
 
-using namespace boost::asio;
+using namespace boost;
+using namespace boost::asio::ip;
 
 class Client {
 public:
@@ -9,6 +11,17 @@ public:
 		Connecting,
 		Connected
 	};
+	
+	Client(boost::asio::io_context& io_context);
+
 private:
 	State connectionState;
+	asio::io_context& io_context;
+	udp::socket& socket;
+	udp::endpoint receiverEndpoint;
+	udp::endpoint senderEndpoint;
+	array<char, 64> sendBuf;
+	array<char, 64> recvBuf;
+
+	void ResolveReceiverEndpoint();
 };
